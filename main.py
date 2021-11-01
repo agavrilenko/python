7232cbfdd2fcc2759c53f6bb9ee24dfdd9d93d44
Alexey Gavrilenko <trash@inbox.ru>
2021-11-01 17:25:08 +0000

    added lambda for lists comprehension

--- main.py
+++ main.py
@@ -14,3 +14,13 @@ def by_two(item):
   return item*2
 
 print(list(map(by_two, [1,2,3,4])))
+print(list(map(lambda item: item**2,[1,2,3,4,5])))
+
+a = [(0,1), (4,6), (-10,5)]
+a.sort(key = lambda key: key[1])
+print(a)
+
+even_list = [item**2 for item in range(0,100) 
+if item %2 == 0]
+print(even_list)
+
