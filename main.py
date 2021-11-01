f551a5cb097bda9d8e8dd7a113df8db192ba4830
Alexey Gavrilenko <trash@inbox.ru>
2021-11-01 19:59:37 +0000

    Decorators

--- main.py
+++ main.py
@@ -1,3 +1,38 @@
+
+#decorator - interseptor
+def another_dec_func(func):
+  def wrapper(*args, **kwargs):
+    print('****WRAPPING*****')
+    func(*args,**kwargs)
+    print('****END WRAPPING*****')
+  return wrapper
+
+@another_dec_func
+def hello(msg):
+  print(msg)
+
+hello('yahoo')
+
+
+def decorator_function(func):
+  def wrapper():
+    print('******')
+    func()
+    print('*****')
+  return wrapper
+
+@decorator_function
+def hey():
+    print('hello')
+
+hey()
+
+dup_list = ['a','b','c','c','b','d','e']
+duplicates = list( {v for v in dup_list if dup_list.count(v)>1 })
+print(duplicates)
+
+
+
 basket = [1,2,3,4,5,5]
 s = set(basket)
 print(type({1,2,3}))
@@ -23,4 +58,10 @@ print(a)
 even_list = [item**2 for item in range(0,100) 
 if item %2 == 0]
 print(even_list)
-
+# same idea for set but use {} instead of []
+# dictionary
+my_dict = {'a': 2, 'b':5}
+new_dict = {k:v**2 for k,v in my_dict.items() if v%2==1}
+print(new_dict)
+new_dict = {str(k):k*2 for k in [1,2,3,4,5]}
+print(new_dict)
